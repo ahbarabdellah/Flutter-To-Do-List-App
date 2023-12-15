c5060f0eddc2c07362e1836630c0935397fcb728
Ahbar Abdellah <ahbarabdellah12@gmail.com>
2023-12-15 20:21:41 +0100

    implimenting UI is done !

---
+++ lib/colors/AppColors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Floral White: Use for light backgrounds and subtle accents
+  static const Color floralWhite = Color.fromARGB(255, 191, 248, 255);
+
+  // Timberwolf: Ideal for neutral backgrounds and interface elements
+  static const Color timberwolf = Color(0xFFCCC5B9);
+
+  // Black Olive: Great for dark mode UI and text elements
+  static const Color blackOlive = Color(0xFF403D39);
+
+  // Eerie Black: Suitable for dark themes and contrast elements
+  static const Color eerieBlack = Color(0xFF252422);
+
+  // Flame: Perfect for attention-grabbing buttons and highlights
+  static const Color flame = Color(0xFFEB5E28);
+
+  // Add more colors with their usage comments here if needed
+}
